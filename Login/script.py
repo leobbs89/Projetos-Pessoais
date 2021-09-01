b367ae0b0e331e1a63094615e3dfe5f257588cda
leobbs89 <leobbs89@gmail.com>
2021-09-01 18:04:47 -0300

    Add main function and some minors corrections

--- Login/script.py
+++ Login/script.py
@@ -1,41 +1,54 @@
-class User():
-    """Mantain user information"""
+import pandas as pd
+
+
+class User:
+    """Maintain user information"""
 
     def __init__(self, name, id, password):
         self.name = name
         self.id = id
         self.password = password
 
-    def check_password(self,password):
-        if self.password == password :
+    def check_password(self, password):
+        if self.password == password:
             return True
         else:
             return False
 
-#Insert user in DataFrame
-def insert_user(user,df):
-    df = df.append({"NAME":user.name,"ID":user.id,"PASSWORD":user.password},ignore_index=True,sort=True)
+
+# Insert user in DataFrame
+def insert_user(user: User, df: pd.DataFrame) -> pd.DataFrame:
+    df = df.append({"NAME": user.name, "ID": user.id, "PASSWORD": user.password}, ignore_index=True, sort=True)
     df = df.sort_values("NAME")
     return df
 
-#Checks if ID is in DataFrame
-def check_id(id,df):
-    if id in df["ID"].tolist() :
+
+# Checks if ID is in DataFrame
+def check_id(id, df):
+    if id in df["ID"].tolist():
         return True
     else:
         return False
 
-#Return user from DataFrame
-def return_user(id,df):
-    user_rown = df.loc[df.index[df["ID"] == "camposka88"]]
-    user = User(user_rown["NAME"],user_rown["ID"],user_rown["PASSWORD"])
+
+# Return user from DataFrame
+def return_user(id, df):
+    user_rown = df.loc[df.index[df["ID"] == "id"]]
+    user = User(user_rown["NAME"], user_rown["ID"], user_rown["PASSWORD"])
     return user
 
-#Create DataFrame
-import pandas as pd
-df = pd.DataFrame(columns = ["NAME","ID","PASSWORD"])
-user1 = User("Leandro","leobbs89","ima450")
-user2 = User("Carolina","camposka88","carol1812")
-df = insert_user(user1,df)
-df = insert_user(user2,df)
-print(df)
+
+# MAIN FUNCTION
+def main() -> int:
+    # Create DataFrame
+    df = pd.DataFrame(columns=["NAME", "ID", "PASSWORD"])
+    user1 = User("Leandro", "leobbs89", "ima450")
+    user2 = User("Carolina", "camposka88", "carol1812")
+    df = insert_user(user1, df)
+    df = insert_user(user2, df)
+    print(df)
+    return 0
+
+
+if __name__ == "__main__":
+    main()
